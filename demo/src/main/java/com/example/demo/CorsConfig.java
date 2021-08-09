a9a37b79d238226debc7df4448c3b9e853e6fe0c
frank <weijie.chen@polyvirtual.com>
2021-08-09 17:20:16 +0800

    添加请求

---
+++ demo/src/main/java/com/example/demo/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.web.cors.CorsConfiguration.ALL;
+
+/**
+ * CORS configuration
+ */
+@Configuration
+public class CorsConfig  implements WebMvcConfigurer {
+
+
+    /**
+     * 开启跨域
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 设置允许跨域的路由
+        registry.addMapping("/**")
+                // 设置允许跨域请求的域名
+                .allowedOriginPatterns("*")
+                // 是否允许证书（cookies）
+                .allowCredentials(true)
+                // 设置允许的方法
+                .allowedMethods("*")
+                // 跨域允许时间
+                .maxAge(3600);
+    }
+
+}
